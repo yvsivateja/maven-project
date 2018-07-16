566f6f875411cc9c15b2ed1e740b9763f86cf3c7
Siva Teja (healthAlliance) <TejaS@HEALTHCARE.HUARAHI.HEALTH.GOVT.NZ>
2018-07-16 16:06:34 +1200

    new junit

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -20,7 +20,7 @@ public class HelloMavenTest extends FunctionalTestCase {
 
 	@Test
 	public void retrieveFlightsAddsAppropriateHeader() throws Exception {
-		MuleEvent event = runFlow("retrieveFlights");
+		MuleEvent event = runFlow("retrieveFlightss");
 		String contentType = event.getMessage().getOutboundProperty("Content-Type");
 		assertEquals("application/json", contentType);
 	}
